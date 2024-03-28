409a68b64eae8d8bffb484e1ba03eff222fb3df2
Isaac V <ivcafe413@gmail.com>
2024-03-28 18:25:49 -0500

    Using underscores and trying to rebuild for debug

--- Environment/robot.gd
+++ Environment/robot.gd
@@ -10,22 +10,22 @@ var drive_direction = Vector2.ZERO
 func _ready():
 	pass # Replace with function body.
 	
-func _integrate_forces(state):
-	if Input.is_action_pressed("swerve_left"):
-		rotation_direction = -1.0
-	if Input.is_action_pressed("swerve_right"):
-		rotation_direction = 1.0
+func _integrate_forces(_state):
+	#if Input.is_action_pressed("swerve_left"):
+		#rotation_direction = -1.0
+	#if Input.is_action_pressed("swerve_right"):
+		#rotation_direction = 1.0
 		
 	apply_torque(rotation_direction * torque)
 	
-	if Input.is_action_pressed("drive_up"):
-		drive_direction.y = -1
-	if Input.is_action_pressed("drive_down"):
-		drive_direction.y = 1
-	if Input.is_action_pressed("drive_left"):
-		drive_direction.x = -1
-	if Input.is_action_pressed("drive_right"):
-		drive_direction.x = 1
+	#if Input.is_action_pressed("drive_up"):
+		#drive_direction.y = -1
+	#if Input.is_action_pressed("drive_down"):
+		#drive_direction.y = 1
+	#if Input.is_action_pressed("drive_left"):
+		#drive_direction.x = -1
+	#if Input.is_action_pressed("drive_right"):
+		#drive_direction.x = 1
 		
 	if drive_direction.length() > 0:
 		drive_direction = drive_direction.normalized()
@@ -33,5 +33,5 @@ func _integrate_forces(state):
 		apply_central_impulse(drive_direction * drive_torque)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
