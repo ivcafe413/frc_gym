695a0b526f9a8db289690478b34067d83022a530
Isaac V <ivcafe413@gmail.com>
2024-03-28 14:32:54 -0500

    Updating test loop

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ import os
 serverIP = '127.0.0.1'
 serverPort = '8888'
 
-# projectPath = os.path.join(os.getcwd(), "Environment")
-# projectPath = "Environment"
-# godotPath = "C:\\godot\\bin\\godot.windows.editor.x86_64.exe"
-# godotPath = '"C:/Program Files/Godot/Godot_v4.2-stable_win64.exe" --upwards environment.tscn'
-# scenePath = "--upwards environment.tscn"
-# projectPath = "project.godot"
-# exeCmd = "{} {}".format(godotPath, scenePath)
 exeCmd = "Environment/frc_run.x86_64"
 
 actionSpace = spaces.Box(low=np.array([-1.0, -1.0, -1.0], dtype=np.float32), # Linear X-Axis, Linear Y-Axis, Rotational X-Axis
@@ -55,13 +48,12 @@ env = gym.make('server-v0', serverIP=serverIP, serverPort=serverPort, exeCmd=exe
                action_space=actionSpace, observation_space=observationSpace,
                window_render=True, renderPath=renderPath)
 
-new_action = env.action_space.sample()
-
-env.reset()
-for i in range(0, 5):
+state = env.reset()
+for _ in range(1000):
+    env.render()
+    new_action = env.action_space.sample()
     print(env.step(new_action))
 
 env.render()
 
 env.close()
-del env
